c8052fa9dfc275dd7f4cb560f18001d3028ef516
lanetteammonika <lanetteam.monikaa@gmail.com>
2017-11-23 18:03:28 +0530

    facebook integration done

--- TenderWatch/app/src/main/java/com/example/lcom48/tenderwatch/MainActivity.java
+++ TenderWatch/app/src/main/java/com/example/lcom48/tenderwatch/MainActivity.java
@@ -2,50 +2,55 @@ package com.example.lcom48.tenderwatch;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-public TextView aboutapp;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView aboutapp;
     Context context;
+    private Button btnContractor,btnClient;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        aboutapp = (TextView) findViewById(R.id.aboutapp);
         context=this;
-        //aboutapp.setOnClickListener(new onClickListe);
-        aboutapp.setOnClickListener(new View.OnClickListener() {
+        InitView();
+        InitListener();
 
-            @Override
-            public void onClick(View view) {
-                //final Dialog dialog = new Dialog(MainActivity.this);
+
+    }
+
+    private void InitListener() {
+        btnContractor.setOnClickListener(this);
+        btnClient.setOnClickListener(this);
+        aboutapp.setOnClickListener(this);
+    }
+
+    private void InitView() {
+        btnContractor = (Button) findViewById(R.id.btn_contractor);
+        btnClient = (Button) findViewById(R.id.btn_client);
+        aboutapp = (TextView) findViewById(R.id.aboutapp);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.aboutapp:
                 final Dialog dialog=new Dialog(context,R.style.full_screen_dialog);
-                //setting custom layout to dialog
                 dialog.setContentView(R.layout.aboutapp);
 
-                //dialog.setTitle("Custom Dialog");
-                //LinearLayout ll = (LinearLayout) findViewById(R.id.relativeLayout1);
-               // ll.setAlpha();
-                //View backgroundImage = findViewById(R.id.relativeLayout1);
-                //Drawable background = backgroundImage.getBackground();
-                //background.setAlpha(80);
-//                adding text dynamically
-//                TextView txt = (TextView) dialog.findViewById(R.id.textView);
-//                txt.setText("Put your dialog text here.");
-//
-//                ImageView image = (ImageView)dialog.findViewById(R.id.image);
-//                image.setImageDrawable(getResources().getDrawable(android.R.drawable.ic_dialog_info));
-//
-//                //adding button click event
                 Button dismissButton = (Button) dialog.findViewById(R.id.button_close);
                 dismissButton.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -53,8 +58,22 @@ public TextView aboutapp;
                         dialog.dismiss();
                     }
                 });
+
+                WebView mWebView = (WebView) dialog.findViewById(R.id.about_webview);
+
+                WebSettings webSettings = mWebView.getSettings();
+                webSettings.setJavaScriptEnabled(true);
+                mWebView.loadUrl("file:///android_res/raw/about.htm");
                 dialog.show();
-            }
-        });
+                break;
+            case R.id.btn_contractor:
+                intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_client:
+                intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
